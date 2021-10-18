81b9ce703d212bb80be1f9f89f41b01845e0d4eb
Mariana Vettorazzo <marianavettorazzo@gmail.com>
2021-10-17 23:03:00 -0300

    Ajustanto tipo transferencia

---
+++ main/main.go
@@ -0,0 +1,67 @@
+package main
+
+import "exemple.com/DesafioStone/desafio_stone/http"
+
+func main() {
+
+	http.ConfiguringServer()
+
+}
+
+// O desafio será avaliado através de quatro critérios.
+
+// Entrega
+// O resultado final está completo para ser executado?
+// O resultado final atende ao que se propõe fazer?
+// O resultado final atende totalmente aos requisitos propostos?
+// O código possui algum controle de dependências?
+// Boas Práticas
+// O código está de acordo com o guia de estilo do Go?
+// O código está bem estruturado?
+// O código está fluente na linguagem?
+// O código faz o uso correto de Design Patterns?
+// Documentação
+// O código foi entregue com um arquivo de README claro de como se guiar?
+// O código possui comentários pertinentes?
+// O código está em algum controle de versão?
+// Os commits são pequenos e consistentes?
+// As mensagens de commit são claras?
+// Código Limpo
+// O código possibilita expansão para novas funcionalidades?
+// O código é Don't Repeat Yourself?
+// O código é fácil de compreender?
+// O código possui testes?
+// Material de Estudo
+// Go
+// Aprenda Go com Testes
+// Curso Aprenda Go (@ellenkorbes)
+// Learn Go
+// Gophercise
+// Build Web Application with Golang
+// Error Handling
+// DDD Lite in Go
+// Repository Pattern in Go
+// Rest
+// Desing RESTful API's
+// HTTP Status Code
+// Boas praticas
+// Boas Práticas na Stone
+// Uber-go Guide
+// Outros
+// SOLID
+// SOLID in GO
+// Grupo de Estudos de Go (pt-br)
+// Web Development with Go (samples) - Jon Calhoun
+// Go Bootcamp from Gopherguides.tv
+// Just for Func
+// Go WEB Examples
+// Dave Cheney Blog
+// Ardan Labs Blog
+// Comunidade Go
+// Slack Gopher
+// Telegram
+// Sugestões
+// Gorilla Mux
+// Negroni
+// Chi
+// sirupsen/log
